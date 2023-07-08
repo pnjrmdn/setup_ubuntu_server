e8ec5f5a9b829ce99f7edeb24e96b01086083c57
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-07-08 17:54:07 +0800

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+sudo apt-get install git
+
+javac --verison
+
+apt-get install openjdk-11-jdk-headless
+
+# download jenkins 
+# copy link adress lalu wget pada direktori
+www.jenkins.io/download
+
+# copy link adress lalu wget pada direktori
+
+# start jenkins
+java -jar jenkins.war --httpPort=9090 &
